ed9f738ee48a3c2e4bccb803151f961fbdfdb36a
myf19 <myf1995@gmail.com>
2018-01-13 13:19:00 +0100

    refactored 3 tests

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -10,8 +10,12 @@ public class Calculator {
         if(input.length() == 1){
             return stringToInt(input);
         } else {
-            return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+            return getSum(numbers[0], numbers[1]);
         }
+    }
+
+    private int getSum(String numA, String numB){
+        return Integer.parseInt(numA) + Integer.parseInt(numB);
     }
 
     private boolean isEmpty(String input){
